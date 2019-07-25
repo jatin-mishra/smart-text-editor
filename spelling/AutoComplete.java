7066d9a191275ee92038cebff315c19b4c7e8900
Jatin Mishra <44506861+jatin-mishra@users.noreply.github.com>
2019-07-25 23:49:19 +0530

    Update AutoComplete.java

--- spelling/AutoComplete.java
+++ spelling/AutoComplete.java
@@ -5,10 +5,7 @@ package spelling;
 
 import java.util.List;
 
-/**
- * @author Christine
- *
- */
+
 public interface AutoComplete {
 	public List<String> predictCompletions(String prefix, int numCompletions);
 }
