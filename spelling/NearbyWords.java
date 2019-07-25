a6e5fe87c4e18e315a4c92e4cfc80cb0fe0551ad
Jatin Mishra <44506861+jatin-mishra@users.noreply.github.com>
2019-07-25 23:45:39 +0530

    Update NearbyWords.java

--- spelling/NearbyWords.java
+++ spelling/NearbyWords.java
@@ -8,11 +8,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
-
-/**
- * @author UC San Diego Intermediate MOOC team
- *
- */
 public class NearbyWords implements SpellingSuggest {
 	// THRESHOLD to determine how many words to look through when looking
 	// for spelling suggestions (stops prohibitively long searching)
